57a01461eb63d18839539c9c74e2452f1a5790a9
Carlos Florian <carlosgflorian@gmail.com>
2026-06-26 18:43:06 -0500

    chore: clean up obsolete LevelRendererMixin, register END_MAIN hook, and setup Java 25 toolchain

--- src/client/java/com/easywp/client/EasyWpClient.java
+++ src/client/java/com/easywp/client/EasyWpClient.java
@@ -1,18 +1,16 @@
 package com.easywp.client;
 
 import net.fabricmc.api.ClientModInitializer;
-
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
-import net.minecraft.client.KeyMapping;
-import com.mojang.blaze3d.platform.InputConstants;
-import net.minecraft.network.chat.Component;
-import org.lwjgl.glfw.GLFW;
+import net.fabricmc.fabric.api.client.rendering.v1.level.LevelRenderEvents;
 
 public class EasyWpClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		ModKeyBindings.register();
 		WaypointRenderer.init();
+
+		LevelRenderEvents.END_MAIN.register(context -> {
+			WaypointRenderer.render(context);
+		});
 	}
 }
