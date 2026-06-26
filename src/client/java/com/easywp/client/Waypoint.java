40cc4262374a8b471c5f3bdaecadf486b0f2f4cc
Carlos Florian <carlosgflorian@gmail.com>
2026-06-26 18:42:31 -0500

    refactor: update Waypoint model with getters and setters

--- src/client/java/com/easywp/client/Waypoint.java
+++ src/client/java/com/easywp/client/Waypoint.java
@@ -3,19 +3,41 @@ package com.easywp.client;
 import net.minecraft.core.BlockPos;
 
 public class Waypoint {
-    private final String name;
-    private final BlockPos pos;
+    private String name;
+    private BlockPos pos;
+    private int color; // ARGB color
 
-    public Waypoint(String name, BlockPos pos){
-        this.name = name;
-        this.pos = pos;
+    public Waypoint(String name, BlockPos pos) {
+        this(name, pos, 0xFF00FF00); // Default to green (ARGB: green is 0xFF00FF00)
     }
 
-    public String getName(){
+    public Waypoint(String name, BlockPos pos, int color) {
+        this.name = name;
+        this.pos = pos;
+        this.color = color;
+    }
+
+    public String getName() {
         return name;
     }
 
-    public BlockPos getPos(){
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BlockPos getPos() {
         return pos;
+    }
+
+    public void setPos(BlockPos pos) {
+        this.pos = pos;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
     }
 }
